c4cf4014305803d38f1b380b6d25a7cd8ecba421
visualstudio <visualstudio@122-PC19>
2019-03-13 11:13:51 -0400

    TestMethod

---
+++ Unit test/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTestProject1
+{
+    [TestMethod]
+    public void Debit_WhenAmountIsMoreThanBalance_ShouldThrowArgumentOutOfRange()
+    {
+        // Arrange
+        double beginningBalance = 11.99;
+        double debitAmount = 20.0;
+        BankAccount account = new BankAccount("Mr.Bryan Walton", beginingBalance);
+
+        // Act
+        try
+        {
+            account.Debit(debitAmount);
+        }
+        catch (ArgumentOutOfRangeException e)
+        {
+            // Assert
+            StringAssert.Contains(e.Message, BankAccount.DebitAmountExceedsBalanceMessage);
+            return;
+        }
+        Assert.Fail("The expected expected was not thrown.");
+    }
+}
